9e572d21a4a9d3248f492a0af71ac71b547b504e
UnInterstedLedge <91570921+UnInterstedLedge@users.noreply.github.com>
2023-04-28 16:13:40 -0500

    Update inventory.py

--- inventory.py
+++ inventory.py
@@ -1,10 +1,42 @@
-class Inventory:
-    def __init__(self) -> None:
-        self.__barcode: int = -1
-        self.__description: str = ""
-        self.__price: float = -1
-        self.__stock: int = -1
+# Inventory class
 
-    def addToCart() -> None:
-        pass
-    
+import numpy as np
+import pandas as pd
+from Book import Book
+
+class Inventory:
+  def __init__(self):
+    self.inventoryFile = 'InventoryList.csv'
+    self.inventoryList = pd.read_csv(self.inventoryFile)
+
+  ## adds quantity of items back to inventory, this function gets called in Cart class
+  ## when item is deleted from Cart
+  def addBackQuantity(self, ISBN, quantity):
+    self.inventoryList.loc[self.inventoryList['ISBN'] == ISBN, 'Quantity'] += int(quantity)
+    self.inventoryList.to_csv(self.inventoryFile, encoding='utf-8', index=False)
+
+  
+  def delInventory(self,ISBN,quantity):
+    self.inventoryList.loc[self.inventoryList.ISBN == ISBN, "Quantity"] -= int(quantity)
+    self.inventoryList.to_csv(self.inventoryFile, encoding='utf-8', index=False)
+  def printInventory(self):
+    book = Book()
+    inventory_list = book.readBookFile.merge(self.inventoryList, on='ISBN')
+    print(inventory_list)
+    print("######## Inventory List ########\n")
+    print(inventory_list)
+
+  def checkItemQuantity(self, ISBN, quantity):
+      isbn_filtered = self.inventoryList[self.inventoryList['ISBN'] == ISBN]
+      
+      if not isbn_filtered.empty:
+          # Convert the 'Quantity' column to integer
+          isbn_filtered = isbn_filtered.copy()
+          isbn_filtered.loc[:, 'Quantity'] = isbn_filtered['Quantity'].astype(int)
+          
+          if isbn_filtered.at[isbn_filtered.index[0], 'Quantity'] >= int(quantity):
+              return True
+          else:
+              return False
+      else:
+          return False
